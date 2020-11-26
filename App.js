35ed45946e8947f6e5e87e1202042791a9b7c645
migue1994 <migue24co@gmail.com>
2020-11-26 15:41:17 -0500

    Add dependencies

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import { createStackNavigator } from 'react-navigation-stack';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import CreateAccount from './src/screens/CreateAccount';
+import Menu from './components/DrawerComponent/Menu';
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     Login: LoginScreen,
     CreateAccount: CreateAccount,
+    Advolang: Menu
   },
   {
-    initialRouteName: 'Home',
+    // initialRouteName: 'Home',
+    initialRouteName: 'Advolang'
   },
 );
 
