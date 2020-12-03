13b460bba703e8421e9a6ac494e4d532f3b5d5f9
migue1994 <migue24co@gmail.com>
2020-12-03 14:53:55 -0500

    Menu button

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DrawerActions, NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Menu from "./src/components/DrawerComponent/Menu";
+import { StatusBar } from "expo-status-bar";
+import { Button, Text } from "react-native";
 
 export default function App() {
   const Stack = createStackNavigator();
+
+
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="menu">
+      <StatusBar barStyle="dark-content" backgroundColor="#2576AD" />
+      <Stack.Navigator 
+        initialRouteName="menu"
+      >
         <Stack.Screen
           name="menu"
           component={Menu}
-          options={{
+          options={({navigation}) => ({
             title: "Welcome to Advolang!",
             headerStyle: {
               backgroundColor: "#2576AD",
             },
             headerTintColor: "#fff",
-            headerTitleAlign:'center',
+            headerTitleAlign: "center",
             headerTitleStyle: {
               fontWeight: "bold",
             },
-          }}
+            headerLeft: () => (
+              <Button
+                onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+                title={'menu'}
+              />
+            )
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/components/DrawerComponent/Menu.js
+++ src/components/DrawerComponent/Menu.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import NewRecommendation from "../newRecommendationComponent/NewRecommendation";
 import Languages from "../LanguagesComponent/Languages";
@@ -8,12 +7,12 @@ const Drawer = createDrawerNavigator();
 
 export default function Menu() {
   return (
-    <Drawer.Navigator initialRouteName={"New Recommendation"}>
-      <Drawer.Screen
-        name={"New Recommendation"}
-        component={NewRecommendation}
-      />
-      <Drawer.Screen name={"Languages"} component={Languages} />
-    </Drawer.Navigator>
+      <Drawer.Navigator initialRouteName={"New Recommendation"}>
+        <Drawer.Screen
+          name={"New Recommendation"}
+          component={NewRecommendation}
+        />
+        <Drawer.Screen name={"Languages"} component={Languages} />
+      </Drawer.Navigator>
   );
 }
