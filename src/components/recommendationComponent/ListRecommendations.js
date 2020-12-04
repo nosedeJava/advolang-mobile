4600b72dccf208eeb7b7941c046982b80ca18e3d
Stilink <stilink306@gmail.com>
2020-12-03 20:31:55 -0500

    Connected to back

--- src/components/recommendationComponent/ListRecommendations.js
+++ src/components/recommendationComponent/ListRecommendations.js
@@ -3,14 +3,12 @@ import Recommendation from "./Recommendation";
 import {View, ScrollView} from "react-native";
 
 export default function ListRecommendations(props) {
-
     const listContent = props.recommendations.map((recommendation, i) => {
         return <Recommendation key={"recomItem-" +i}{...recommendation} nav={props.nav} />;
     });
 
     return (
         <View style={{width: "100%"}} >
-
             {listContent}
         </View>
     );
